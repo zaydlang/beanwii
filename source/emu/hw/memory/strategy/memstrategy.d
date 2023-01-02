40e496f704c3bbdb8c82338443cf466ef914f4a2
SmallBeanMachine <zayd@qumsieh.net>
2023-01-01 16:52:32 -0800

    added slowmem read functions

--- source/emu/hw/memory/strategy/memstrategy.d
+++ source/emu/hw/memory/strategy/memstrategy.d
@@ -1,7 +1,10 @@
 module emu.hw.memory.strategy.memstrategy;
 
 import emu.hw.disk.dol;
+import emu.hw.memory.strategy.slowmem.slowmem;
 import util.number;
+
+alias Mem = SlowMem;
 
 interface MemStrategy {
     public void map_dol(WiiDol* dol, u8[] partition_data);
--- source/emu/hw/memory/strategy/slowmem/slowmem.d
+++ source/emu/hw/memory/strategy/slowmem/slowmem.d
@@ -5,12 +5,31 @@ import emu.hw.memory.strategy.memstrategy;
 import util.log;
 import util.number;
 
+// NOTE: this memstrategy is VERY slow! It's very simplistic, and only
+// here for basic testing purposes.
 final class SlowMem : MemStrategy {
+    struct Mapping {
+        u32 address;
+        u8[] data;
+    }
+
+    public bool in_range(Mapping mapping, u32 address) {
+        return address >= mapping.address && address < mapping.address + mapping.data.length;
+    }
+
+    public T read_be(T)(Mapping mapping, u32 address) {
+        assert(mapping.in_range(address));
+        return mapping.data.read_be!T(address);
+    }
+
+    Mapping[] mappings;
+
     override public void map_dol(WiiDol* dol, u8[] partition_data) {
         for (int i = 0; i < WII_DOL_NUM_TEXT_SECTIONS; i++) {
             u32 text_address = cast(u32) dol.text_address[i];
             u32 text_offset  = cast(u32) dol.text_offset[i];
             u32 text_size    = cast(u32) dol.text_size[i];
+            mappings ~= Mapping(text_address, partition_data[text_offset .. text_offset + text_size]);
 
             log_disk("Mapping text section %d at 0x%08X, offset 0x%08X, size 0x%08X", i, text_address, text_offset, text_size);
         }
@@ -19,12 +38,25 @@ final class SlowMem : MemStrategy {
             u32 data_address = cast(u32) dol.data_address[i];
             u32 data_offset  = cast(u32) dol.data_offset[i];
             u32 data_size    = cast(u32) dol.data_size[i];
+            mappings ~= Mapping(data_address, partition_data[data_offset .. data_offset + data_size]);
 
             log_disk("Mapping text section %d at 0x%08X, offset 0x%08X, size 0x%08X", i, data_address, data_offset, data_size);
         }
 
         log_disk("BSS addr: %x", cast(u32) dol.bss_address);
         log_disk("BSS size: %x", cast(u32) dol.bss_size);
+        mappings ~= Mapping(cast(u32) dol.bss_address, new u8[cast(u32) dol.bss_size]);
+
         log_disk("Entrypoint: %x", cast(u32) dol.entry_point);
+    }
+
+    T read_be(T)(u32 address) {
+        foreach (mapping; mappings) {
+            if (mapping.in_range(address)) {
+                return mapping.read_be!T(address);
+            }
+        }
+
+        error_slowmem("Read from invalid address 0x%08X", address);
     }
 }
