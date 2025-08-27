1b71d0203fbe61c4a7f0ca2feac7d3b3806b9146
zaydlang <zayd@qumsieh.net>
2025-08-27 09:30:50 -0700

    dumb fix

--- tools/install_app/install_app.sh
+++ tools/install_app/install_app.sh
@@ -1,4 +1,4 @@
 sudo udisksctl mount -b /dev/sdb1
 sudo mkdir -p /run/media/root/DOLPHIN/apps/$1
-sudo cp $2  /run/media/root/DOLPHIN/apps/boot.dol
+sudo cp $2  /run/media/root/DOLPHIN/apps/$1/boot.dol
 sudo umount /run/media/root/DOLPHIN
