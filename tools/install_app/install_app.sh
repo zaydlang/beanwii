4100a71a13d9eccbec03dd280f557888fe29d52d
zaydlang <zayd@qumsieh.net>
2025-08-29 23:12:53 -0700

    make tool slightly easeir to use

--- tools/install_app/install_app.sh
+++ tools/install_app/install_app.sh
@@ -2,3 +2,4 @@ sudo udisksctl mount -b /dev/sdb1
 sudo mkdir -p /run/media/root/DOLPHIN/apps/$1
 sudo cp $2  /run/media/root/DOLPHIN/apps/$1/boot.dol
 sudo umount /run/media/root/DOLPHIN
+sudo rm -rf /run/media/root/DOLPHIN
