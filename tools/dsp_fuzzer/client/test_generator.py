5d04476cf894be5a8fba9e97536f27034819133c
zaydlang <zayd@qumsieh.net>
2025-08-30 21:50:14 -0700

    more test case generation fanciness

--- tools/dsp_fuzzer/client/test_generator.py
+++ tools/dsp_fuzzer/client/test_generator.py
@@ -4,13 +4,33 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 
 import assembler
 import fuzz
+import random
+
+def r(low, high):
+    return random.randint(low, high)
+
+def unused(bitlength):
+    return random.randint(0, (1 << bitlength) - 1)
 
 def sanity():
     assembler.nop()
 
+def add():
+    assembler.add(r(0, 1), unused(8))
+
 test_cases = [
-    sanity
+    sanity,
+    add
 ]
 
+test_cases = [tc for tc in test_cases if tc.__name__ in sys.argv[2] or len(sys.argv) == 2]
+
+if len(test_cases) == 0:
+    print("No test cases matched the filter.")
+    exit(0)
+
 for test_case in test_cases:
+    print("Generating test case:", test_case.__name__)
     fuzz.send_to_wii(sys.argv[1], f"source/test/dsp/tests/{test_case.__name__}.bin", *fuzz.do_tests(test_case, 1000))
+
+print("All done!")
